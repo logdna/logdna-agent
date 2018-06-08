e707917e00f38c8d3c39c8c9d01f8ebf3ba6c29c
Samir Musali <samir.musali@logdna.com>
2018-06-08 13:17:06 -0400

    Update: Windows Event Tail is working

--- scripts/windows/winTail.ps1
+++ scripts/windows/winTail.ps1
@@ -1,14 +1,21 @@
 Set-PSDebug -Strict
-function WinEventTail($LogName) {
-    $curr = (Get-WinEvent -log $LogName -max 1).RecordId
-    while ($true)
+$LogName = $args[0]
+$curr = (Get-WinEvent -log $LogName -max 1 2> $null).RecordId
+if ([string]::IsNullOrEmpty($curr))
+{
+	$curr = -1
+}
+while ($true)
+{
+    start-sleep -Seconds 1
+    $next = (Get-WinEvent -log $LogName -max 1 2> $null).RecordId
+    if ([string]::IsNullOrEmpty($next))
     {
-        start-sleep -Seconds 1
-        $next = (Get-WinEvent -log $LogName -max 1).RecordId
-        if ($next -gt $curr) {
-            $data = Get-WinEvent -log $LogName -max ($next - $curr + 1000) | where {$_.RecordId -gt $curr}
-            $data
-            $curr = $data[-1].RecordId
-        }
+    	$next = -1
+    }
+    if ($next -gt $curr) {
+        $data = Get-WinEvent -log $LogName -max ($next - $curr + 1000) 2> $null | where {$_.RecordId -gt $curr}
+        $data
+        $curr = $data[-1].RecordId
     }
 }
