306eb8d10e47bf770ae5ca0f3be98d41e1044531
Samir Musali <samir.musali@gmail.com>
2018-06-14 21:47:30 +0000

    Step-by-Step fixing Automated Test

--- scripts/windows/winTail.ps1
+++ scripts/windows/winTail.ps1
@@ -1,4 +1,8 @@
 Set-PSDebug -Strict
+$file = "\logs\windows-events.log"
+if(!(Test-Path -Path $env:ALLUSERSPROFILE$file)){
+    New-Item -ItemType file -Path $env:ALLUSERSPROFILE$file -Force 2> $null
+}
 $LogName = $args[0]
 $curr = (Get-WinEvent -log $LogName -max 1 2> $null).RecordId
 if ([string]::IsNullOrEmpty($curr))
@@ -14,8 +18,9 @@ while ($true)
     	$next = -1
     }
     if ($next -gt $curr) {
-        $data = Get-WinEvent -log $LogName -max ($next - $curr + 1000) 2> $null | where {$_.RecordId -gt $curr}
+        $data = Get-WinEvent -log $LogName -max ($next - $curr + 1000) 2> $null | where {$_.RecordId -gt $curr} | ConvertTo-Json
         $data
+        $data | out-file $env:ALLUSERSPROFILE$file
         $curr = $data[-1].RecordId
     }
 }
