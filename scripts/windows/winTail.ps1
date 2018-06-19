46783332cb908ea51d3770f580ce443c720aa1bb
Samir Musali <samir.musali@gmail.com>
2018-06-19 22:43:32 +0000

    Tailing is ready

--- scripts/windows/winTail.ps1
+++ scripts/windows/winTail.ps1
@@ -1,21 +1,21 @@
 Set-PSDebug -Strict
 $LogName = $args[0]
-$curr = (Get-WinEvent -FilterHashtable @{LogName=@($LogName)} -MaxEvents 1 2> $null).RecordId
+$curr = (Get-EventLog -LogName $LogName -Newest 1 2> $null).Index
 if ([string]::IsNullOrEmpty($curr))
 {
-	$curr = -1
+    $curr = -1
 }
 while ($true)
 {
     start-sleep -Seconds 1
-    $next = (Get-WinEvent -FilterHashtable @{LogName=@($LogName)} -MaxEvents 1 2> $null).RecordId
+    $next = (Get-EventLog -LogName $LogName -Newest 1 2> $null).Index
     if ([string]::IsNullOrEmpty($next))
     {
-    	$next = -1
+        $next = -1
     }
     if ($next -gt $curr) {
-        $data = Get-WinEvent -FilterHashtable @{LogName=@($LogName)} -MaxEvents ($next - $curr + 1000) 2> $null | where {$_.RecordId -gt $curr}
-        $curr = $data[-1].RecordId
+        $data = Get-EventLog -LogName $LogName -Newest ($next - $curr + 1000) 2> $null | where {$_.Index -gt $curr}
+        $curr = $data[-1].Index
         $data | ConvertTo-Json
     }
 }
