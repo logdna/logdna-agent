6622644c5975ccc39860d8505bd7925d2d98de1e
Samir Musali <samir.musali@logdna.com>
2018-06-21 23:51:29 +0400

    v1.5.4 changes

--- scripts/windows/chocolateyInstall.ps1
+++ scripts/windows/chocolateyInstall.ps1
@@ -1,7 +1,15 @@
 $packageParameters = $env:chocolateyPackageParameters
+$winTailURL = 'https://raw.githubusercontent.com/logdna/logdna-agent/master/scripts/windows/winTail.ps1'
 
 IF(!($packageParameters))
 {
+    if(!(Test-Path -Path $env:ALLUSERSPROFILE\tmp)){
+        New-Item -ItemType directory -Path $env:ALLUSERSPROFILE\tmp
+    }
+    if(Test-Path -Path $env:ALLUSERSPROFILE\tmp\winTail.ps1){
+        Remove-Item $env:ALLUSERSPROFILE\tmp\winTail.ps1
+    }
+    Invoke-WebRequest -Uri $winTailURL -OutFile $env:ALLUSERSPROFILE\tmp\winTail.ps1
     if(!(Test-Path -Path $env:ALLUSERSPROFILE\logs)){
         New-Item -ItemType directory -Path $env:ALLUSERSPROFILE\logs
     }
@@ -9,8 +17,9 @@ IF(!($packageParameters))
         New-Item -ItemType directory -Path $env:ALLUSERSPROFILE\logdna
     }
     cmd.exe /c "nssm.exe install logdna-agent $env:ChocolateyInstall\bin\logdna-agent.exe & exit /b 0"
+    fsutil file createnew $env:ALLUSERSPROFILE\logs\logdna-agent.log 0
     cmd.exe /c "nssm.exe set logdna-agent AppStdout $env:ALLUSERSPROFILE\logs\logdna-agent.log & exit /b 0"
-    cmd.exe /c "nssm start logdna-agent & exit /b 0"
+    cmd.exe /c "nssm.exe set logdna-agent AppStderr $env:ALLUSERSPROFILE\logs\logdna-agent.log & exit /b 0"
 }
 
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\logdna-agent\Parameters"
