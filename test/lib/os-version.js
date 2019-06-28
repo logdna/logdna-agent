6aad166de05499a8b34583efad9c38c39c7bde1d
Samir Musali <samir.musali@logdna.com>
2019-06-28 15:24:37 -0400

    Issue Fix: Missing Asset for Testing - Final

--- test/lib/os-version.js
+++ test/lib/os-version.js
@@ -33,9 +33,10 @@ describe('lib:file-utilities', () => {
     it('Gets the correct OS version', () => {
         return osVersion((error, os) => {
             debug(os);
-            assert.equal(os.name, 'Ubuntu 14.04.4 LTS');
+            assert.equal(os.code, '14.04.4 LTS, Trusty Tahr');
             assert.equal(os.os, 'Ubuntu');
             assert.equal(os.release, '14.04');
+            assert.equal(os.name, 'Ubuntu 14.04.4 LTS');
         });
     });
 
