acbfad9fad8b8bdbed5eccb473de7d4a5d757633
Samir Musali <samir.musali@logdna.com>
2019-07-08 08:58:10 -0700

    Fix in Testing

--- test/lib/file-utilities.js
+++ test/lib/file-utilities.js
@@ -123,7 +123,7 @@ describe('lib:file-utilities', () => {
                     path: true
                 }, cb);
             }, (config, cb) => {
-                return cb(fileUtilities.saveConfig(config, path.join(tempDir, 'test.config')));
+                return fileUtilities.saveConfig(config, path.join(tempDir, 'test.config'), cb);
             }, (success, cb) => {
                 return properties.parse(configPath, {
                     path: true
