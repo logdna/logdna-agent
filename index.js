3090509e22ac0fb37373a0817e3de8d853fd97f0
vilyapilya <vikzak1988@gmail.com>
2019-07-18 22:29:47 -0700

    Allow the execution if the config file does not exist yet.  (#116)
    * vilya
    
    * run lint fix
    
    * clean
    
    * Remove the extra string
    
    * move the logic where it was
    
    * Add error check
    
    * change log to console.log
    
    * change
    
    * ESLinted
    
    * refactor the anon functions
    
    * fix typo
    
    * replace existsSyn with asyn acees method
    
    * fix lint

--- index.js
+++ index.js
@@ -74,219 +74,237 @@ program
     })
     .parse(process.argv);
 
-if ((os.platform() === 'win32' && require('is-administrator')()) || process.getuid() <= 0) {
-    async.waterfall([(cb) => {
-        return properties.parse(config.CONF_FILE, {
-            path: true
-        }, cb);
-    }, (parsedConfig, cb) => {
-        parsedConfig = parsedConfig || {};
 
-        // allow key to be passed via env
-        if (process.env.LOGDNA_AGENT_KEY || process.env.AGENT_KEY || process.env.INGESTION_KEY) {
-            parsedConfig.key = process.env.LOGDNA_AGENT_KEY;
+const checkFileExistence = (cb) => {
+    fs.access(config.CONF_FILE, (error) => {
+        if (error) {
+            cb(null, null);
+        } else {
+            properties.parse(config.CONF_FILE, {
+                path: true
+            }, cb);
         }
+    });
+};
 
-        if (!program.key && !parsedConfig.key) {
-            console.error('LogDNA Ingestion Key not set! Use -k to set or use environment variable LOGDNA_AGENT_KEY.');
-            process.exit();
-        }
+const mainExecution = (parsedConfig, cb) => {
+    parsedConfig = parsedConfig || {};
 
-        // allow exclude to be passed via env
-        if (process.env.LOGDNA_EXCLUDE || process.env.EXCLUDE) {
-            parsedConfig.exclude = process.env.LOGDNA_EXCLUDE;
-        }
+    // allow key to be passed via env
+    if (process.env.LOGDNA_AGENT_KEY || process.env.AGENT_KEY || process.env.INGESTION_KEY) {
+        parsedConfig.key = process.env.LOGDNA_AGENT_KEY;
+    }
 
-        // allow exclude regex to be passed via env
-        if (process.env.LOGDNA_EXCLUDE_REGEX || process.env.EXCLUDE_REGEX) {
-            parsedConfig.exclude_regex = process.env.LOGDNA_EXCLUDE_REGEX;
-        }
+    if (!program.key && !parsedConfig.key) {
+        console.error('LogDNA Ingestion Key not set! Use -k to set or use environment variable LOGDNA_AGENT_KEY.');
+        process.exit();
+    }
 
-        if (process.env.USEJOURNALD) {
-            parsedConfig.usejournald = process.env.USEJOURNALD;
-        }
+    // allow exclude to be passed via env
+    if (process.env.LOGDNA_EXCLUDE || process.env.EXCLUDE) {
+        parsedConfig.exclude = process.env.LOGDNA_EXCLUDE;
+    }
 
-        // sanitize
-        if (!parsedConfig.logdir) {
-            parsedConfig.logdir = [config.DEFAULT_LOG_PATH]; // default entry
-        } else if (!Array.isArray(parsedConfig.logdir)) {
-            parsedConfig.logdir = parsedConfig.logdir.split(','); // force array
-        }
+    // allow exclude regex to be passed via env
+    if (process.env.LOGDNA_EXCLUDE_REGEX || process.env.EXCLUDE_REGEX) {
+        parsedConfig.exclude_regex = process.env.LOGDNA_EXCLUDE_REGEX;
+    }
 
-        if (parsedConfig.exclude && !Array.isArray(parsedConfig.exclude)) {
-            parsedConfig.exclude = parsedConfig.exclude.split(',');
-        }
+    if (process.env.USEJOURNALD) {
+        parsedConfig.usejournald = process.env.USEJOURNALD;
+    }
 
-        var saveMessages = [];
+    // sanitize
+    if (!parsedConfig.logdir) {
+        parsedConfig.logdir = [config.DEFAULT_LOG_PATH]; // default entry
+    } else if (!Array.isArray(parsedConfig.logdir)) {
+        parsedConfig.logdir = parsedConfig.logdir.split(','); // force array
+    }
 
-        if (program.key) {
-            parsedConfig.key = program.key;
-            saveMessages.push('Your LogDNA Ingestion Key has been successfully saved!');
-        }
+    if (parsedConfig.exclude && !Array.isArray(parsedConfig.exclude)) {
+        parsedConfig.exclude = parsedConfig.exclude.split(',');
+    }
 
-        if (program.set && program.set.length > 0) {
-            for (var i = 0; i < program.set.length; i++) {
-                var kvPair = utils.split(program.set[i], '=', false);
-                if (kvPair.length === 2) {
-                    parsedConfig[kvPair[0]] = kvPair[1];
-                    saveMessages.push('Config variable: ' + kvPair[0] + ' = ' + kvPair[1] + ' been saved to config.');
-                } else {
-                    saveMessages.push('Unknown setting: ' + program.set[i] + '. Usage: -s [key=value]');
-                }
-            }
-        }
+    var saveMessages = [];
 
-        if (program.winevent && program.winevent.length > 0) {
-            if (os.platform() === 'win32') {
-                processed = utils.processOption(program.winevent, parsedConfig.winevent, true);
-                parsedConfig.winevent = processed.values;
-                saveMessages.push('Windows Events: ' + processed.diff + ' been saved to config.');
+    if (program.key) {
+        parsedConfig.key = program.key;
+        saveMessages.push('Your LogDNA Ingestion Key has been successfully saved!');
+    }
+
+    if (program.set && program.set.length > 0) {
+        for (var i = 0; i < program.set.length; i++) {
+            var kvPair = utils.split(program.set[i], '=', false);
+            if (kvPair.length === 2) {
+                parsedConfig[kvPair[0]] = kvPair[1];
+                saveMessages.push('Config variable: ' + kvPair[0] + ' = ' + kvPair[1] + ' been saved to config.');
             } else {
-                saveMessages.push('-w is only available for Windows.');
+                saveMessages.push('Unknown setting: ' + program.set[i] + '. Usage: -s [key=value]');
             }
         }
+    }
 
-        if (program.list) {
-            if (typeof program.list === 'boolean') {
-                program.list = ['all'];
+    if (program.winevent && program.winevent.length > 0) {
+        if (os.platform() === 'win32') {
+            processed = utils.processOption(program.winevent, parsedConfig.winevent, true);
+            parsedConfig.winevent = processed.values;
+            saveMessages.push('Windows Events: ' + processed.diff + ' been saved to config.');
+        } else {
+            saveMessages.push('-w is only available for Windows.');
+        }
+    }
+
+    if (program.list) {
+        if (typeof program.list === 'boolean') {
+            program.list = ['all'];
+        }
+        var conf = properties.parse(fs.readFileSync(config.CONF_FILE).toString());
+        const listResult = utils.pick2list(program.list, conf);
+        if (listResult.valid) {
+            var msg = utils.stringify(listResult.cfg);
+            saveMessages.push(config.CONF_FILE + ':\n' + msg);
+        } else {
+            saveMessages.push(listResult.msg);
+        }
+    }
+
+    if (program.unset && program.unset.length > 0) {
+        const unsetResult = utils.unsetConfig(program.unset, parsedConfig);
+        parsedConfig = unsetResult.cfg;
+        saveMessages.push(unsetResult.msg);
+    }
+
+    if (program.logdir && program.logdir.length > 0) {
+        processed = utils.processOption(program.logdir, parsedConfig.logdir);
+        parsedConfig.logdir = processed.values;
+        saveMessages.push('Log Directories: ' + processed.diff + ' been saved to config.');
+    }
+
+    if (program.logfile && program.logfile.length > 0) {
+        processed = utils.processOption(program.logfile, parsedConfig.logdir);
+        parsedConfig.logdir = processed.values;
+        saveMessages.push('Log Files: ' + processed.diff + ' been saved to config.');
+    }
+
+    if (program.exclude && program.exclude.length > 0) {
+        processed = utils.processOption(program.exclude, parsedConfig.exclude);
+        parsedConfig.exclude = processed.values;
+        saveMessages.push('Exclusions: ' + processed.diff + ' been saved to config.');
+    }
+
+    if (program.excludeRegex) {
+        parsedConfig.exclude_regex = program.excludeRegex;
+        // strip leading and trailing / if exists
+        if (parsedConfig.exclude_regex.substring(0, 1) === '/' && parsedConfig.exclude_regex.substring(parsedConfig.exclude_regex.length - 1) === '/') {
+            parsedConfig.exclude_regex = parsedConfig.exclude_regex.substring(1, parsedConfig.exclude_regex.length - 1);
+        }
+        saveMessages.push('Exclude pattern: /' + parsedConfig.exclude_regex + '/ been saved to config.');
+    }
+
+    if (program.hostname) {
+        parsedConfig.hostname = program.hostname;
+        saveMessages.push('Hostname: ' + parsedConfig.hostname + ' saved to config.');
+    }
+
+    if (program.tags && program.tags.length > 0) {
+        processed = utils.processOption(program.tags, parsedConfig.tags);
+        parsedConfig.tags = processed.values;
+        saveMessages.push('Tags: ' + processed.diff + ' been saved to config.');
+    }
+
+    if (saveMessages.length) {
+        return fileUtils.saveConfig(parsedConfig, config.CONF_FILE, (error, success) => {
+            if (error) {
+                return log(`Error while saving to: ${config.CONF_FILE}: ${error}`);
             }
-            var conf = properties.parse(fs.readFileSync(config.CONF_FILE).toString());
-            const listResult = utils.pick2list(program.list, conf);
-            if (listResult.valid) {
-                var msg = utils.stringify(listResult.cfg);
-                saveMessages.push(config.CONF_FILE + ':\n' + msg);
-            } else {
-                saveMessages.push(listResult.msg);
+
+            for (var i = 0; i < saveMessages.length; i++) {
+                console.log(saveMessages[i]);
             }
-        }
-
-        if (program.unset && program.unset.length > 0) {
-            const unsetResult = utils.unsetConfig(program.unset, parsedConfig);
-            parsedConfig = unsetResult.cfg;
-            saveMessages.push(unsetResult.msg);
-        }
-
-        if (program.logdir && program.logdir.length > 0) {
-            processed = utils.processOption(program.logdir, parsedConfig.logdir);
-            parsedConfig.logdir = processed.values;
-            saveMessages.push('Log Directories: ' + processed.diff + ' been saved to config.');
-        }
-
-        if (program.logfile && program.logfile.length > 0) {
-            processed = utils.processOption(program.logfile, parsedConfig.logdir);
-            parsedConfig.logdir = processed.values;
-            saveMessages.push('Log Files: ' + processed.diff + ' been saved to config.');
-        }
-
-        if (program.exclude && program.exclude.length > 0) {
-            processed = utils.processOption(program.exclude, parsedConfig.exclude);
-            parsedConfig.exclude = processed.values;
-            saveMessages.push('Exclusions: ' + processed.diff + ' been saved to config.');
-        }
-
-        if (program.excludeRegex) {
-            parsedConfig.exclude_regex = program.excludeRegex;
-            // strip leading and trailing / if exists
-            if (parsedConfig.exclude_regex.substring(0, 1) === '/' && parsedConfig.exclude_regex.substring(parsedConfig.exclude_regex.length - 1) === '/') {
-                parsedConfig.exclude_regex = parsedConfig.exclude_regex.substring(1, parsedConfig.exclude_regex.length - 1);
-            }
-            saveMessages.push('Exclude pattern: /' + parsedConfig.exclude_regex + '/ been saved to config.');
-        }
-
-        if (program.hostname) {
-            parsedConfig.hostname = program.hostname;
-            saveMessages.push('Hostname: ' + parsedConfig.hostname + ' saved to config.');
-        }
-
-        if (program.tags && program.tags.length > 0) {
-            processed = utils.processOption(program.tags, parsedConfig.tags);
-            parsedConfig.tags = processed.values;
-            saveMessages.push('Tags: ' + processed.diff + ' been saved to config.');
-        }
-
-        if (saveMessages.length) {
-            return fileUtils.saveConfig(parsedConfig, config.CONF_FILE, (error, success) => {
-                if (error) {
-                    return log(`Error while saving to: ${config.CONF_FILE}: ${error}`);
-                }
-
-                for (var i = 0; i < saveMessages.length; i++) {
-                    console.log(saveMessages[i]);
-                }
-                process.exit(0);
-            });
-        }
-
-        // merge into single var after all potential saveConfigs finished
-        config = Object.assign(config, parsedConfig);
-
-        config.hostname = process.env.LOGDNA_HOSTNAME ||
-                          fs.existsSync('/etc/logdna-hostname') &&
-                          fs.statSync('/etc/logdna-hostname').isFile() &&
-                          fs.readFileSync('/etc/logdna-hostname').toString().trim().replace(HOSTNAME_IP_REGEX, '') ||
-                          config.hostname ||
-                          os.hostname().replace('.ec2.internal', '');
-
-        config.tags = process.env.LOGDNA_TAGS || config.tags;
-
-        if (process.env.LOGDNA_PLATFORM) {
-            config.platform = process.env.LOGDNA_PLATFORM;
-            config.tags = config.tags ? config.tags + ',' + config.platform : config.platform;
-
-            if (config.platform.indexOf('k8s') === 0) {
-                config.RESCAN_INTERVAL = config.RESCAN_INTERVAL_K8S;
-            }
-        }
-
-        return distro(cb);
-    }, (dist, cb) => {
-        if (dist && dist.os) {
-            config.osdist = dist.os + (dist.release ? ' ' + dist.release : '');
-        }
-
-        return request(config.AWS_INSTANCE_CHECK_URL, {
-            timeout: 1000
-            , json: true
-        }, (err, res, body) => {
-            if (res && res.statusCode && body) {
-                config.awsid = body.instanceId;
-                config.awsregion = body.region;
-                config.awsaz = body.availabilityZone;
-                config.awsami = body.imageId;
-                config.awstype = body.instanceType;
-            }
-            return macaddress.all(cb);
+            process.exit(0);
         });
-    }], (error, all) => {
-        if (all) {
-            var ifaces = Object.keys(all);
-            for (var i = 0; i < ifaces.length; i++) {
-                if (
-                    all[ifaces[i]].ipv4 && (
-                        all[ifaces[i]].ipv4.indexOf('10.') === 0 ||
+    }
+
+    // merge into single var after all potential saveConfigs finished
+    config = Object.assign(config, parsedConfig);
+
+    config.hostname = process.env.LOGDNA_HOSTNAME ||
+                    fs.existsSync('/etc/logdna-hostname') &&
+                    fs.statSync('/etc/logdna-hostname').isFile() &&
+                    fs.readFileSync('/etc/logdna-hostname').toString().trim().replace(HOSTNAME_IP_REGEX, '') ||
+                    config.hostname ||
+                    os.hostname().replace('.ec2.internal', '');
+
+    config.tags = process.env.LOGDNA_TAGS || config.tags;
+
+    if (process.env.LOGDNA_PLATFORM) {
+        config.platform = process.env.LOGDNA_PLATFORM;
+        config.tags = config.tags ? config.tags + ',' + config.platform : config.platform;
+
+        if (config.platform.indexOf('k8s') === 0) {
+            config.RESCAN_INTERVAL = config.RESCAN_INTERVAL_K8S;
+        }
+    }
+
+    return distro(cb);
+};
+
+const awsProcessing = (dist, cb) => {
+    if (dist && dist.os) {
+        config.osdist = dist.os + (dist.release ? ' ' + dist.release : '');
+    }
+
+    return request(config.AWS_INSTANCE_CHECK_URL, {
+        timeout: 1000
+        , json: true
+    }, (err, res, body) => {
+        if (res && res.statusCode && body) {
+            config.awsid = body.instanceId;
+            config.awsregion = body.region;
+            config.awsaz = body.availabilityZone;
+            config.awsami = body.imageId;
+            config.awstype = body.instanceType;
+        }
+        return macaddress.all(cb);
+    });
+};
+
+if ((os.platform() === 'win32' && require('is-administrator')()) || process.getuid() <= 0) {
+    async.waterfall([checkFileExistence, mainExecution, awsProcessing]
+        , (error, all) => {
+            if (error) {
+                console.error(error);
+                process.exit();
+            }
+            if (all) {
+                var ifaces = Object.keys(all);
+                for (var i = 0; i < ifaces.length; i++) {
+                    if (
+                        all[ifaces[i]].ipv4 && (
+                            all[ifaces[i]].ipv4.indexOf('10.') === 0 ||
                         all[ifaces[i]].ipv4.indexOf('172.1') === 0 ||
                         all[ifaces[i]].ipv4.indexOf('172.2') === 0 ||
                         all[ifaces[i]].ipv4.indexOf('172.3') === 0 ||
                         all[ifaces[i]].ipv4.indexOf('192.168.') === 0
-                    )
-                ) {
-                    config.mac = all[ifaces[i]].mac;
-                    config.ip = all[ifaces[i]].ipv4 || all[ifaces[i]].ipv6;
-                    break;
+                        )
+                    ) {
+                        config.mac = all[ifaces[i]].mac;
+                        config.ip = all[ifaces[i]].ipv4 || all[ifaces[i]].ipv6;
+                        break;
+                    }
                 }
             }
-        }
 
-        log(program._name + ' ' + pkg.version + ' started on ' + config.hostname + ' (' + config.ip + ')');
+            log(program._name + ' ' + pkg.version + ' started on ' + config.hostname + ' (' + config.ip + ')');
 
-        if (config.platform && config.platform.indexOf('k8s') === 0) {
-            k8s.init();
-        }
+            if (config.platform && config.platform.indexOf('k8s') === 0) {
+                k8s.init();
+            }
 
-        debug('connecting to log server');
-        connectionManager.connectLogServer(config);
-        debug('logdna agent successfully started');
-    });
+            debug('connecting to log server');
+            connectionManager.connectLogServer(config);
+            debug('logdna agent successfully started');
+        });
 } else {
     console.log('You must be an Administrator (root, sudo) run this agent! See -h or --help for more info.');
     process.exit();
