eeae9cead8e816b6c9be2a5638b1c2bd3f3feeb9
Samir Musali <samir.musali@logdna.com>
2019-10-31 20:14:14 -0400

    remove redundant part

--- test/lib/get-distro.js
+++ test/lib/get-distro.js
@@ -14,7 +14,6 @@ describe('lib:getDistro', () => {
     it('Gets the correct Distro', () => {
         return getDistro(testConfig, (error, os) => {
             debug(os);
-            console.log(os);
             assert.equal(os.code, '14.04.4 LTS, Trusty Tahr');
             assert.equal(os.os, 'Ubuntu');
             assert.equal(os.release, '14.04');
