a34de12177772029c61d7c6b636d997c745d1226
Samir Musali <samir.musali@logdna.com>
2019-12-05 14:51:29 -0500

    fix build script

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -1,59 +1,50 @@
 #!/bin/bash
 # Assuming it is running on Debian
 
-# Variables
-ARCH=x64
-DEBIAN=deb
-INPUT_TYPE=dir
-LICENSE=MIT
-NAME=logdna-agent
-NODE_VERSION=12.13.0
-REDHAT=rpm
-
 # Step 1: Install Dependencies
 sudo npm install -g nexe
 sudo apt-get install -y ruby ruby-dev rubygems build-essential rpm
 sudo gem install --no-ri --no-rdoc fpm
 
 # Step 2: Prepare Folders and Files
-mkdir -p .build/scripts
-cp ./scripts/linux/files/* .build/scripts/
+mkdir -p dist/scripts
+cp ./scripts/linux/files/* dist/scripts/
 
 # Step 3: Compile and Build Executable
-nexe -i index.js -o .build/logdna-agent -t linux-${ARCH}-${NODE_VERSION}
+nexe -i index.js -o dist/logdna-agent -t linux-x64-12.13.0
 
 # Step 4: Debian Packaging
 fpm \
-	--input-type ${INPUT_TYPE} \
-	--output-type ${DEBIAN} \
-	--name ${NAME} \
+	--input-type dir \
+	--output-type deb \
+	--name logdna-agent \
 	--version ${VERSION} \
-	--license ${LICENSE} \
+	--license MIT \
 	--vendor "LogDNA, Inc." \
 	--description "LogDNA Agent for Linux" \
 	--url "http://logdna.com/" \
 	--maintainer "support@logdna.com" \
-	--before-remove ./.build/scripts/before-remove \
-	--after-upgrade ./.build/scripts/after-upgrade \
+	--before-remove ./dist/scripts/before-remove \
+	--after-upgrade ./dist/scripts/after-upgrade \
 	--force --deb-no-default-config-files \
-		./.build/logdna-agent=/usr/bin/logdna-agent \
-		./.build/scripts/init-script=/etc/init.d/logdna-agent \
-		./.build/scripts/logrotate=/etc/logrotate.d/logdna-agent
+		./dist/logdna-agent=/usr/bin/logdna-agent \
+		./dist/scripts/init-script=/etc/init.d/logdna-agent \
+		./dist/scripts/logrotate=/etc/logrotate.d/logdna-agent
 
 # Step 5: RedHat Packaging
 fpm \
-	--input-type ${INPUT_TYPE} \
-	--output-type ${REDHAT} \
-	--name ${NAME} \
+	--input-type dir \
+	--output-type rpm \
+	--name logdna-agent \
 	--version ${VERSION} \
-	--license ${LICENSE} \
+	--license MIT \
 	--vendor "LogDNA, Inc." \
 	--description "LogDNA Agent for Linux" \
 	--url "http://logdna.com/" \
 	--maintainer "support@logdna.com" \
-	--before-remove ./.build/scripts/before-remove \
-	--after-upgrade ./.build/scripts/after-upgrade \
+	--before-remove ./dist/scripts/before-remove \
+	--after-upgrade ./dist/scripts/after-upgrade \
 	--force \
-		./.build/logdna-agent=/usr/bin/logdna-agent \
-		./.build/scripts/init-script=/etc/init.d/logdna-agent \
-		./.build/scripts/logrotate=/etc/logrotate.d/logdna-agent
+		./dist/logdna-agent=/usr/bin/logdna-agent \
+		./dist/scripts/init-script=/etc/init.d/logdna-agent \
+		./dist/scripts/logrotate=/etc/logrotate.d/logdna-agent
