0288722de1049130532f75528c2c14a7700a049e
Samir Musali <samir.musali@logdna.com>
2019-12-05 13:52:37 -0500

    fix version script

--- scripts/etc/version.sh
+++ scripts/etc/version.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Variables
-MAJOR_VERSION=1.
-
 # Gather Both Given Versions
 TAG_VERSION=$(git tag --sort=-creatordate | head -n 1)
 PKG_VERSION=$(cat package.json | grep version | cut -d':' -f2 | cut -d '"' -f2)
 
 # Check and Fail if No Match
-if [[ "${TAG_VERSION}" != "${PKG_VERSION}" || ${TAG_VERSION} != ${MAJOR_VERSION}* ]]; then
+if [[ "${TAG_VERSION}" != "${PKG_VERSION}" ]]; then
   echo "There is mismatch:"
   echo "  TAG_VERSION: ${TAG_VERSION}"
   echo "  PKG_VERSION: ${PKG_VERSION}"
