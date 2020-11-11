1e552a88a33253b33ef4f286cccfcdc0f664ce85
Samir Musali <samir.musali@logdna.com>
2020-11-11 16:01:12 -0500

    bump: release LogDNA Agent v2.1.1
    - Cancel timers to prevent shutdown hanging [#215](https://github.com/logdna/logdna-agent/pull/215)

--- tools/files/darwin/logdna-agent.rb
+++ tools/files/darwin/logdna-agent.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 cask "logdna-agent" do
-  version "2.1.0"
-  sha256 "fa6039599c9d3fcb6c33b69c95b7e86bd88261db0d3a73076ea2887876423ded"
+  version "2.1.1"
+  sha256 "235e3fbe1f8899ac6cd0bfe7433dd3c3d254228654e320ff26cc5e710a35b860"
 
   # github.com/logdna/logdna-agent/ was verified as official when first introduced to the cask
   url "https://github.com/logdna/logdna-agent/releases/download/#{version}/logdna-agent-#{version}.pkg"
   appcast "https://github.com/logdna/logdna-agent/releases.atom"
   name "LogDNA Agent"
+  desc "Agent streams from log files to your LogDNA account"
   homepage "https://logdna.com/"
 
   pkg "logdna-agent-#{version}.pkg"
